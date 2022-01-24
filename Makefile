0bb1870a919ccf123f75c00ff64a0d6a4059ccc8
Ulistonee <ulistonee@gmail.com>
2022-01-24 22:44:40 +0300

    need to fix wp

--- Makefile
+++ Makefile
@@ -3,17 +3,17 @@
 all:
 	#mkdir -p /Users/rchalmer/data/db
 	#mkdir -p /Users/rchalmer/data/wp
-	docker compose -f srcs/docker-compose.yml build
-	docker compose -f srcs/docker-compose.yml up -d
+	docker-compose -f srcs/docker-compose.yml build
+	docker-compose -f srcs/docker-compose.yml up -d
 
 
 up:
-	docker compose -f srcs/docker-compose.yml up -d
+	docker-compose -f srcs/docker-compose.yml up -d
 
 
 down:
-	docker compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down
 
 
 ps:
-	docker compose -f srcs/docker-compose.yml ps
+	docker-compose -f srcs/docker-compose.yml ps
---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "im running..."
+PASSWDDB='1234'
+MAINDB='wordpress'
+USER='rchalmer'
+
+#nginx -g 'daemon off;'
+openrc default
+/etc/init.d/mariadb setup
+rc-service mariadb start
+#/usr/bin/mysqld_safe
+mysql -e "CREATE DATABASE ${MAINDB}"
+mysql -e "CREATE USER ${USER}@'%' IDENTIFIED BY '${PASSWDDB}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+#mysql wordpress < /home/wordpress.sql
+#mysql -h mysql-svc:3306 -u admin --password='1234' wordpress < ./wordpress.sql
+#/usr/bin/supervisord -c /etc/supervisord.conf
+rc-service mariadb stop
+mysqld_safe
+#sh
